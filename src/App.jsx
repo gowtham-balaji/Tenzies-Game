b6b61ba6177982973de4c7d8cab963a2e4251140
gowtham-balaji <gowthambalaji02890@gmail.com>
2023-03-06 22:05:34 +0530

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,87 @@
- import { useEffect, useState } from "react"
- import Die from "./Die"
- import "./styles.css"
- import { nanoid } from 'nanoid'
- import Confetti from "react-confetti"
- 
-
- export default function App(){
-
-  
-  const [dice ,setDice] = useState(newArray())
-  const[tenzies ,setTenzies] = useState(false)
+import { useEffect, useState } from "react"
+import Die from "./Die"
+import "./styles.css"
+import { nanoid } from 'nanoid'
+import Confetti from "react-confetti"
 
 
-    useEffect(() => {
-      const held = dice.every(die => die.held)
-     const firstValue = dice[0].value
-     const allSameValue = dice.every(die => die.value === firstValue)
-     if(held && allSameValue){
+export default function App() {
+
+
+  const [dice, setDice] = useState(newArray())
+  const [tenzies, setTenzies] = useState(false)
+
+
+  useEffect(() => {
+    const held = dice.every(die => die.held)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value === firstValue)
+    if (held && allSameValue) {
       setTenzies(true)
       console.log("you won")
-     }
-     },[dice])
+    }
+  }, [dice])
 
-  function newArray(){
+  function newArray() {
     const newArr = []
-    for(let i=0;i<10;i++){
+    for (let i = 0; i < 10; i++) {
       newArr.push(generateroll())
     }
     return newArr
   }
 
-  function generateroll(){
-    return{
-      value : Math.ceil(Math.random() * 6),
-          held:false,
-          id:nanoid()
+  function generateroll() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      held: false,
+      id: nanoid()
     }
   }
-  function rollDice(){
-    if(!tenzies){
-      setDice(prevDice => prevDice.map(die =>{
-        return  die.held ? die : 
-       
-       generateroll()
+  function rollDice() {
+    if (!tenzies) {
+      setDice(prevDice => prevDice.map(die => {
+        return die.held ? die :
+
+          generateroll()
       }))
     }
-    else{
-     setDice(newArray())
-     setTenzies(false)
+    else {
+      setDice(newArray())
+      setTenzies(false)
     }
   }
 
-  function holdDice(id){
+  function holdDice(id) {
     setDice(prevDice => prevDice.map(die => {
-      if(die.id === id){
-       return  {...die , held: !die.held}
+      if (die.id === id) {
+        return { ...die, held: !die.held }
       }
-      else{
+      else {
         return die
       }
     }))
   }
 
   const diceElements = dice.map(die => {
-    return <Die value = {die.value} key = {die.id} held ={die.held} holdDice ={() => {holdDice(die.id)}}/>
+    return <Die value={die.value} key={die.id} held={die.held} holdDice={() => { holdDice(die.id) }} />
   })
 
 
-  return(
+  return (
     <>
-     <div className="confetti">{tenzies && <Confetti />}</div>
-    <h1 className="header">Tenzies Game</h1>
-   
-    <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="confetti">{tenzies && <Confetti />}</div>
+      <h1 className="header">Tenzies Game</h1>
+
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <main>
-     <div className="container">
-        {diceElements}
-      </div>
-      <button className="btn" onClick={rollDice}>
-        {tenzies ? "New Game" : "Roll"}
-      </button>
-     </main>
+        <div className="container">
+          {diceElements}
+        </div>
+        <button className="btn" onClick={rollDice}>
+          {tenzies ? "New Game" : "Roll"}
+        </button>
+      </main>
     </>
- 
+
   )
- }
+}
--- src/Die.jsx
+++ src/Die.jsx
@@ -2,15 +2,15 @@
 import React from "react";
 
 
-function Die(props){
+function Die(props) {
 
    const styles = {
-      background : props.held  ? "#37db21" : "#fff"
-     }
+      background: props.held ? "#37db21" : "#fff"
+   }
    return (
-    <div style = {styles} onClick = {props.holdDice} className="die">
-    <h1>{props.value}</h1>
-   </div>
+      <div style={styles} onClick={props.holdDice} className="die">
+         <h1>{props.value}</h1>
+      </div>
    )
 }
 
